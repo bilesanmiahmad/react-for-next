1402e0ac09c2fa836260549abc913f9eccaed9a7
Ahmad Bilesanmi <fbilesanmi@gmail.com>
2024-11-23 00:07:03 +0000

    react refresher

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Post from './components/Post'
+
+function App() {
+  return (
+    <main>
+      <Post author="Maxmilian" body="React.js is awesome"/>
+      <Post author="Manuel" body="Check out the course"/>
+      <Post author="Marian" body="More react tutorials"/>
+    </main>
+  );
+}
+
+export default App
